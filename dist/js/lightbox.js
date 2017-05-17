811dbaaac45c71762781e97c164ff06531c14d1e
Truc Nguyen <nguyencongtruc@vn.oro.com>
2017-05-17 16:40:44 +0700

    update lightbox slider

--- dist/js/lightbox.js
+++ dist/js/lightbox.js
@@ -3,53 +3,137 @@
 
   var Lightbox = $.lightbox = (function () {
     var
+      $win,
+      winWidth,
+      minWidth = 960,
 
       slideIndex = 0,
-      $slides = null,
-      $thumbnail = null,
+      slideLength = 0,
+      slideLengthBefore = 0,
+      posWrapper = 0,
+      widthWrapper = 0,
+      widthSlide = 960,
+      current = 0,
 
       $lightbox = null,
-      $modalContent = null;
+      $lightboxItem = null,
+      $lightboxSlider = null,
+      $lightboxWrapper = null,
+      $lightboxSliderItem = null;
 
 
     function init() {
+      $win = $(window);
+      winWidth = $win.width();
+
       $lightbox = $('.lightbox');
-      $slides = $('.lightbox_slider_item');
-      $modalContent = $('.lightbox_slider');
+      $lightboxItem = $('.lightbox_item');
+      $lightboxSlider = $('.lightbox_slider');
+      $lightboxWrapper = $('.lightbox_slider_wrapper');
+      $lightboxSliderItem = $('.lightbox_slider_item');
+
+      // Clone First and Last Slide Append to Wrapper.
+      var firstSlide = $lightboxSlider.find(".lightbox_slider_item:first-child").clone();
+      var lastSlide = $lightboxSlider.find(".lightbox_slider_item:last-child").clone();
+      $lightboxWrapper
+        .append(firstSlide)
+        .prepend(lastSlide);
+      slideLengthBefore = $lightboxSliderItem.length;
+      slideLength = $('.lightbox_slider_item').length;
+
       lightbox();
-      paginationSlide();
+      resize();
+      $win.on('resize', function (e) {
+        resize();
+      });
     }
 
-    function slideshow(n) {
-      slideIndex += n;
-      if(slideIndex >= $slides.length) {
-        slideIndex = 0;
-      } else if(slideIndex < 0) {
-        slideIndex = $slides.length - 1;
+    function resize() {
+      winWidth = $win.width();
+      winWidth = winWidth < 767 ? winWidth : winWidth = widthSlide;
+      $lightboxSlider.css({
+        "width": winWidth + "px"
+      });
+      $lightboxWrapper.find(".lightbox_slider_item").css({
+        "width": winWidth + "px"
+      });
+      posWrapper = -(slideIndex * winWidth + winWidth);
+      $lightboxWrapper.css({
+        "width": slideLength * winWidth + "px",
+        "marginLeft": posWrapper + "px"
+      });
+    }
+
+    function slideshow(next) {
+      var nextPosition = checkPosition(next);
+      $lightboxWrapper.animate({
+        'marginLeft': nextPosition + 'px'
+      }, 500);
+      slideIndex = next;
+      for(var i = 0; i < slideLengthBefore; i++) {
+        $lightboxSliderItem.eq(i).removeClass('active')
       }
-      for(var i = 0; i < $slides.length; i++) {
-        $slides.eq(i).css('display', 'none');
-      }
-      $slides.eq(slideIndex).css('display', 'block');
+      $lightboxSliderItem.eq(slideIndex).addClass('active')
     }
 
     function paginationSlide() {
-      $('.next').click(function () {
-        slideshow(1);
-      });
-      $('.prev').click(function () {
-        slideshow(-1);
-      });
+      $('.lightbox_slider_next, .lightbox_slider_prev')
+        .off()
+        .on('click', function (e) {
+          e.preventDefault();
+          e.stopPropagation();
+          if($lightboxWrapper.is(":animated")) {
+            return;
+          }
+          var next;
+          if($(this).hasClass("lightbox_slider_next")) {
+            next = (slideIndex + 1) > slideLengthBefore - 1 ? 0 : slideIndex + 1;
+          } else {
+            next = (slideIndex - 1) < 0 ? slideLengthBefore - 1 : slideIndex - 1;
+          }
+          slideshow(next);
+        });
+    }
+
+    function checkPosition(next) {
+      widthSlide = winWidth < 767 ? widthSlide = winWidth : widthSlide;
+      var nextPosition = -(widthSlide * next + widthSlide);
+      if(slideIndex === slideLengthBefore - 1 && next === 0) {
+        $lightboxWrapper.css('marginLeft', 0);
+        return -(widthSlide);
+      } else if(slideIndex === 0 && next === slideLengthBefore - 1) {
+        $lightboxWrapper.css({
+          'marginLeft': -(slideLengthBefore * widthSlide + widthSlide) + 'px'
+        });
+        return -(slideLengthBefore * widthSlide);
+      } else {
+        return nextPosition;
+      }
     }
 
     function lightbox() {
-      $('.lightbox').each(function (index, el) {
-        var $item = $(this).find('.lightbox_item');
+      $('.lightbox_item').each(function (index, el) {
+        var $item = $(this);
+        slideLength = $('.lightbox_slider_item').length;
+        widthWrapper = slideLength * widthSlide;
+        $lightboxWrapper.css('width', widthWrapper);
         $item.on('click', function (e) {
-          var current = $(this).index();
-          slideIndex = current; // Set Current Item When CLick
-          slideshow(0);
           e.preventDefault();
+          var current = $(this).index();
+          posWrapper = -(current * widthSlide) - widthSlide;
+          var posWrapperSp = -(current * winWidth) - winWidth;
+          if(winWidth > 767) {
+            $lightboxWrapper.css({
+              'marginLeft': posWrapper + 'px'
+            });
+          } else {
+            $lightboxWrapper.css({
+              'marginLeft': posWrapperSp + 'px'
+            });
+          }
+          slideIndex = current;
+          paginationSlide();
+
           $(this).stop().toggleClass('open');
           if($(this).hasClass('open')) {
             $(this).parent().next().addClass('lightbox_slider_show').find('.lightbox_slider_container').addClass('show');
@@ -57,7 +141,7 @@
             $('body').append('<div class="lightbox_slider_overlay"></div>');
             closeLightBox();
           } else {
-            $(this).closest('html').find('.lightbox_slider.' + id).removeClass('lightbox_slider_show').find('.lightbox_slider_container').removeClass('show');
+            $(this).closest('html').find('.lightbox_slider').removeClass('lightbox_slider_show').find('.lightbox_slider_container').removeClass('show');
             $('.lightbox_slider_overlay').remove();
           }
         });
@@ -67,9 +151,10 @@
     function closeLightBox() {
       $('.lightbox_slider_overlay').off().on('click', function (e) {
         e.preventDefault();
-        $('.lightbox_item').removeClass('open');
-        $modalContent.removeClass('lightbox_slider_show').find('.lightbox_slider_container').removeClass('show');
+        $lightboxItem.removeClass('open');
+        $lightboxSlider.removeClass('lightbox_slider_show').find('.lightbox_slider_container').removeClass('show');
         $('.lightbox_slider_overlay').remove();
+        $lightboxWrapper.css('marginLeft', 0);
       });
     }
     return {
